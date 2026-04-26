1548c05c62efa91f9dc9500c5e25dcb6f8df6428
roma20ix <138139934+roma20ix@users.noreply.github.com>
2026-04-26 20:55:20 +0700

    add 0.py

---
+++ solve/0.py
@@ -0,0 +1,18 @@
+"""
+Во время решения задач можно сделать для каждой задачи отдельный файл, но на 
+это уйдёт больше времени, особенно при повторной проверке, поэтому лучше
+писать все решения в одном файле, комментируя предыдущие.
+
+Шаблон для кода с библиотеками
+"""
+
+from math import ceil, log2, floor, sqrt
+from fnmatch import fnmatch
+from functools import lru_cache
+from collections import defaultdict
+from turtle import *
+from sys import setrecursionlimit
+from itertools import permutations, product
+from ipaddress import ip_address, ip_network
+import csv
+import re
