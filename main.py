244de7d4324224a5206a42491f8f443c3cdd69a1
roma20ix <138139934+roma20ix@users.noreply.github.com>
2026-04-26 14:34:56 +0700

    add main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+A repository with theory for self-preparation for the Unified State Exam in computer science
+P.S: Read the file README.md
+
+
+                                                        ____    ___   _
+                         _ __   ___   _ __ ___    __ _ |___ \  / _ \ (_)__  __
+                        | '__| / _ \ | '_ ` _ \  / _` |  __) || | | || |\ \/ /
+                        | |   | (_) || | | | | || (_| | / __/ | |_| || | >  <
+                        |_|    \___/ |_| |_| |_| \__,_||_____| \___/ |_|/_/\_\
+
+
+Author: Roman Ivanov (roma20ix)
+Created: 26.04.2026
+Link to Github: [https://github.com/roma20ix/EGE]
+"""
